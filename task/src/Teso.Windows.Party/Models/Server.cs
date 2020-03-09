c6ffbbb42e0b0e94532fb7b60aea407d54f93930
gienec <g.necionis@gmail.com>
2020-03-10 00:57:18 +0200

    Add distance units.

--- task/src/Teso.Windows.Party/Models/Server.cs
+++ task/src/Teso.Windows.Party/Models/Server.cs
@@ -1,8 +1,16 @@
-﻿namespace Teso.Windows.Party.Models
+﻿using System;
+
+namespace Teso.Windows.Party.Models
 {
     public class Server
     {
         public string Name { set; get; }
-        public string Distance { set; get; }
+
+        private string _distance;
+        public string Distance
+        {
+            set => _distance = value;
+            get => $"{_distance} km";
+        }
     }
 }
